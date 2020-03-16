13bf69bdfc05f5a2a5da0aa7d5211c75711f6ab6
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-16 18:51:06 -0400

    Title Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
    */
   head: {
     // titleTemplate: "%s - " + process.env.npm_package_name,
-    title: "BingeWorthy Shows",
+    title: "BingeWorthy - Show Ratings",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
