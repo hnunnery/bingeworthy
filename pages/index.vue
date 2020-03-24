cc1263834473fbd014531fc634343c800b304e9c
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-24 00:47:59 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -245,8 +245,8 @@
           </v-row>
 
           <!-- START - MOBILE - RATINGS CARDS -->
-          <v-row v-show="!loading && this.search" class="hidden-md-and-up justify-center mb-6">
-            <v-col cols="12" sm="6" v-for="rating in filteredRatings" :key="rating.id" class="pb-0">
+          <v-row v-show="!loading && this.search" class="hidden-md-and-up justify-center mt-0 mb-6">
+            <v-col cols="12" sm="6" v-for="rating in filteredRatings" :key="rating.id" class="py-0">
               <v-card
                 class="px-2 ma-1 align-center d-flex"
                 color="rgba(17, 17, 17, 0.5)"
