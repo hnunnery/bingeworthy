772c527d1c9d143241ef22b123a3c3ac799ede70
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-31 15:22:28 -0400

    Update AccountOptions.vue

--- components/AccountOptions.vue
+++ components/AccountOptions.vue
@@ -20,7 +20,7 @@
               <v-btn
                 large
                 to="/signup"
-                @click="dialog=false;"
+                @click="this.$emit('click'); dialog=false;"
                 active-class="no-active"
                 class="text-capitalize mx-2 my-3 px-5"
                 style="background-color: rgba(120, 47, 64, 0.5);"
@@ -30,7 +30,7 @@
               <v-btn
                 large
                 to="/signin"
-                @click="dialog=false;"
+                @click="this.$emit('click'); dialog=false;"
                 active-class="no-active"
                 class="text-capitalize mx-2 my-3 px-5"
                 style="background-color: rgba(120, 47, 64, 0.5);"
@@ -52,7 +52,7 @@
               <v-btn
                 large
                 to="/updatename"
-                @click="dialog=false;"
+                @click="this.$emit('click'); dialog=false;"
                 active-class="no-active"
                 class="text-capitalize mx-2 my-3 px-6"
                 style="background-color: rgba(120, 47, 64, 0.5);"
@@ -87,6 +87,7 @@ export default {
       if (confirm("Sign Out?")) {
         this.$store.dispatch("logout");
         this.$router.push("/");
+        this.$emit('click'); 
         this.dialog = false;
       }
     }
