2a8afb92a774824e95c7877e8b2c9b3dd52644c3
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-31 09:21:46 -0400

    Update SpeedDial.vue

--- components/SpeedDial.vue
+++ components/SpeedDial.vue
@@ -31,7 +31,7 @@
     <v-btn v-show="userAuth" fab small color="primary" to="/" active-class="accent secondary--text">
       <v-icon>mdi-home</v-icon>
     </v-btn>
-    <AccountOptions />
+    <AccountOptions @click="fab=false" />
   </v-speed-dial>
 </template>
 
