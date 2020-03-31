6d8f45effc2da9050c16562807d28531dfad4f95
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-31 11:24:41 -0400

    Update SignIn.vue

--- pages/SignIn.vue
+++ pages/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid class="pt-0">
-    <v-row class="justify-center align-center" style="min-height: 70vh !important;">
+    <v-row class="justify-center align-center" style="min-height: 70vh !important; margin-top: 10vh">
       <v-col cols="12" sm="8" md="6" style="max-width: 600px;">
         <v-card class="pa-6 px-lg-10" color="rgba(17, 17, 17, 0.5)" elevation="15">
           <form @submit.prevent="onSignIn">
