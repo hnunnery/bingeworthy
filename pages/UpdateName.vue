6ba4df9993fba007340e3140c44c4e8b32f93c0e
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-03-31 16:13:30 -0400

    Update UpdateName.vue

--- pages/UpdateName.vue
+++ pages/UpdateName.vue
@@ -1,12 +1,7 @@
 <template>
   <v-container fluid class="pt-0">
     <v-row class="justify-center align-center" style="height: 65vh !important;">
-      <v-col cols="12" class="hidden-xs-only mt-0">
-        <h1
-          class="secondary--text text-center font-weight-bold font-italic mt-1 mb-2"
-          style="letter-spacing: 2px; font-size: 6vmax;"
-        >BingeWorthy</h1>
-      </v-col>
+      <v-col cols="12" class="hidden-xs-only mt-10">
       <v-col cols="12" sm="8" md="6" style="max-width: 600px;">
         <v-card class="pa-6 px-lg-10" color="rgba(17, 17, 17, 0.5)" elevation="15">
           <form @submit.prevent="updateDisplayName">
